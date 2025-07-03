1769e2b0710a7d3337bd1b6bc895481b570597c7
Pratyush krishna <pk201929@gmail.com>
2025-07-03 21:13:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+    id: 1, 
+    name: "Agra",
+    info: "Agra is a city in northern India, known for the iconic Taj Mahal The Taj Mahal is a beautiful white marble mausoleum located in Agra, India. It was built by the Mughal Emperor Shah Jahan in memory of his beloved wife Mumtaz Mahal, who died in 1631. Construction began the same year and was completed around 1648. ",
+    image:"https://cdn.thecodehelp.in/Agra.jpeg",
+    price:"35,758" ,
+    },
+
+    {
+        id: 2, 
+        name: "Jaipur",
+        info: "Jaipur, also known as the Pink City, is the capital of Rajasthan, India. It was founded in 1727 by Maharaja Sawai Jai Singh II. The city is famous for its rich history, royal palaces, forts, and vibrant culture. Many of its buildings are painted pink, a color symbolizing hospitality, which gave the city its nickname. Popular tourist attractions in Jaipur include the Hawa Mahal, City Palace, Amber Fort, and Jantar Mantar, a UNESCO World Heritage Site. Jai",
+        image:"https://cdn.thecodehelp.in/Jaipur.jpeg",
+        price:"50,000" ,
+    },
+
+     {
+            id: 3, 
+            name: "Goa",
+            info: "Goa is a small coastal state in western India, known for its beautiful beaches, vibrant nightlife, and rich Portuguese heritage. It was a Portuguese colony for over 450 years before becoming part of India in 1961. The capital of Goa is Panaji, and its largest city is Vasco da Gama. Goa attracts tourists from around the world with its sun-kissed beaches, historic churches, lively festivals, and delicious seafood. It’s a perfect blend of Indian and Eur.",
+            image:"https://cdn.thecodehelp.in/Goa.jpeg",
+            price:"55,358" ,
+      },
+         {
+                id: 4, 
+                name: "Varanasi",
+                info: "Varanasi, also known as Kashi or Banaras, is one of the oldest living cities in the world, located on the banks of the river Ganga in Uttar Pradesh, India. It is considered one of the holiest cities in Hinduism and a major center for spiritual learning, rituals, and temples. Pilgrims from across the country visit to take a holy dip in the Ganga and perform religious ceremonies. The city is also known for its beautiful ghats, ancient Kashi Vishw",
+                image:"https://cdn.thecodehelp.in/Varanasi.jpeg",
+                price:"67,765" ,
+         },
+         {
+            id: 5, 
+            name: "Darjeeling",
+            info: "Darjeeling is a beautiful hill station located in the state of West Bengal, India. Nestled in the Himalayas, it is often called themQueen of the Hills due to its breathtaking landscapes, pleasant climate, and lush greenery. The town is famous for its tea plantations, which produce some of the finest tea in the world. Darjeeling Tea is internationally known for its unique flavor and aroma.",
+            image:"https://cdn.thecodehelp.in/Darjeeling.jpeg",
+            price:"95,758" ,
+
+         },
+
+         {
+            id: 6, 
+            name: "Jaisalmer",
+            info: "Jaisalmer, also known as the Golden City, is a beautiful town located in the heart of the Thar Desert in Rajasthan, India. It gets its name from the golden-yellow sandstone used in its buildings, which glow in the sunlight. The city was founded by Raja Rawal Jaisal in 1156 AD. Its most famous landmark is the Jaisalmer Fort, one of the few living forts in the world, where people still live and work.",
+            image:"https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+            price:"15,688" ,
+            },
+
+            {
+                id: 7, 
+                name: "kochi",
+                info: "Kochi, also known as Cochin, is a vibrant port city located on the southwest coast of Kerala, India. Known as the Queen of the Arabian Sea, Kochi has a rich history influenced by the Portuguese, Dutch, and British. It was one of the first European colonies in India. ",
+                image:"https://cdn.thecodehelp.in/Kochi.jpeg",
+                price:"57,432" ,
+                },
+
+
+
+
+
+
+
+]
+export default data;
